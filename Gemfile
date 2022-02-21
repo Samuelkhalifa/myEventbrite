db0eb3f8921b70d1cf3e7f40996c29ca2e21f1c5
Samuelkhalifa <97511680+Samuelkhalifa@users.noreply.github.com>
2022-02-21 17:15:15 +0100

    Commit_9_Projet_en_cours_S7J1_(6)

--- Gemfile
+++ Gemfile
@@ -69,7 +69,7 @@ gem 'table_print'
 gem 'faker'
 gem 'better_errors'
 gem 'letter_opener' 
-gem 'letter_opener_web'
+gem 'dotenv'
 gem 'dotenv-rails'
 
 ##########################################
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+
+
+  
+  #config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.perform_deliveries = true
 
 
 
